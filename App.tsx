1da54f963552fbd09bcc7ac93f43dfab38f7cd7a
adamoui63 <62793906+adamoui63@users.noreply.github.com>
2022-12-04 20:39:53 +0000

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,14 @@
+import Navbar from './Navbar';
+import Home from './Home';
+import * as React from 'react';
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="content">
+        <Home />
+      </div>
+    </div>
+  );
+}
+export default App;
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-r3tuue
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-yxsyua)
