7500d3b2ce2e62caa4334aa791f8f46f2a39eb81
adamoui63 <62793906+adamoui63@users.noreply.github.com>
2022-12-05 13:48:10 +0000

    2 files modified, 1 added

--- Home.js
+++ Home.js
@@ -1,9 +1,20 @@
 import * as React from 'react';
+import {useState} from 'react';
 
 function Home(){
+  const[blogs,setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ]);
   return (
       <div className = "home">
-       <h1> Home Page </h1>
+       {blogs.map((blog) => 
+         <div className = 'blog-preview' key = {blog.id}>
+           <h2> {blog.title} </h2>
+           <p> Written by {blog.author} </p>
+           </div>
+       )}
        </div>
   );
 }
